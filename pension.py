50904fcafed427285918b2903b15b938d80e9009
Githubaments <flynnshane@gmail.com>
2021-01-04 20:21:50 +0000

    Update pension.py

--- pension.py
+++ pension.py
@@ -70,7 +70,8 @@ df.iat[0,-1] = 0
 st.write(df)
 
 
-year_surpass = df.query('Inflow > Annual Growth').index[0]
+year_surpass = df.query('Growth > Inflow').index[0]
+
 st.write(year_surpass)
 
 fig = px.area(df_area)
